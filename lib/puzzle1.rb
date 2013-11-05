7edb626978741cf8c6415c6634d760a9f0213379
Allison <asheren@neighborsations.com>
2013-11-05 15:22:14 -0500

    wrote regex for currency. Figured out part of the hash that needs to be made.

--- lib/puzzle1.rb
+++ lib/puzzle1.rb
@@ -4,49 +4,48 @@ require 'nokogiri'
 require 'csv'
 
 class Puzzle1 #class is a method that defines puzzle and actually creates a constant that points to the puzzle object
-  #so even if you don't have methods defined, something is still done
+  #so even if you don't have methods defined, something is still done (it returns nil)
 
 
 
 #put the csv file into a hash and then find the DM1182s
   def find_item
-    skus = {}
+    
 
-    CSV.foreach('/Users/allisonsheren/projects/coder_night/lib/SAMPLE_TRANS.csv', {:headers => true, 
+      CSV.foreach('/Users/allisonsheren/projects/coder_night/lib/SAMPLE_TRANS.csv', {:headers => true, 
                                                                                    :return_headers => true, 
                                                                                    :header_converters => :symbol, 
                                                                                    :converters => :all} ) do |row|
-    #puts row[0]
-    puts skus["DM1182"][:amount] #right now, referring to skus the hash, needs to be skus the column
-    end
+    #puts row[1] 
+    skus = {
+      "DM1182" => [:amount]
+    }
+    p skus
+    #puts skus["DM1182"][:amount] #right now, referring to skus the hash, needs to be skus the column
+      end
   end
 
   def item
-  # item = []
-   # if @item = AUD 
+   arr = []
+    if @amount == /(\ AUD\z)/
       # convert AUD to USD
       ##the nokogiri stuff goes somewhere in here
-       # doc = Nokogiri::XML(open('/Users/allisonsheren/projects/coder_night/lib/SAMPLE_RATES.xml'))
-       # doc.xpath("//AUD").each do |node|
-      #   puts node.text
-         # end 
-   # else
-      # puts @item 
-
+        doc = Nokogiri::XML(open('/Users/allisonsheren/projects/coder_night/lib/SAMPLE_RATES.xml'))
+        doc.xpath("//AUD").each do |node|
+          puts node.text
+        end 
+    else
+       puts @amount 
+    end
+    arr << @amount
   end
 
-  # def total_sales
-      #total_sales = []
-  #   @item.reduce(:+)
-  # end
-  
-
-
-  
- #
-	
+  def total_sales
+    @amount.reduce(:+)
+  end
 end
 
 p = Puzzle1.new
 p.find_item #calls the method. You need to set p (a variable that refers to an object Puzzle1) to Puzzle1.new. We created the class
 #puzzle1 which created a puzzle object but in order to call the method we have to creat an actual object to call it on 
+p.item 
