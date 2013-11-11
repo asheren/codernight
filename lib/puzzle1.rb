66a45ad06cef2a86ff1317b2f1cbb24a213e1a5f
Allison <asheren@neighborsations.com>
2013-11-10 20:38:00 -0500

    cleaned up code for final submission

--- lib/puzzle1.rb
+++ lib/puzzle1.rb
@@ -1,43 +1,59 @@
 require 'nokogiri'
-#require_relative 'SAMPLE_RATES.xml'
+require 'open-uri'
+require 'active_support/core_ext/hash/conversions'
 
 require 'csv'
+require 'pry'
 
-class Puzzle1 #class is a method that defines puzzle and actually creates a constant that points to the puzzle object
-  #so even if you don't have methods defined, something is still done (it returns nil)
+class Puzzle1 
 
 
 
 #put the csv file into a hash and then find the DM1182s
-  def find_item
-    
+  def load_amounts
 
-      CSV.foreach('/Users/allisonsheren/projects/coder_night/lib/SAMPLE_TRANS.csv', {:headers => true, 
-                                                                                   :return_headers => true, 
-                                                                                   :header_converters => :symbol, 
-                                                                                   :converters => :all} ) do |row|
-    #puts row[1] 
-    skus = {
-      "DM1182" => [:amount] #how to get this to put the actual amount from the CSV?
-    }
-    p skus
-    #puts skus["DM1182"][:amount] #right now, referring to skus the hash, needs to be skus the column
+      csv = CSV.open('/Users/allisonsheren/projects/coder_night/lib/SAMPLE_TRANS.csv', :headers => true,  
+                                                                                      :header_converters => :symbol)
+      skus = {}
+      csv.each do |row| #http://technicalpickles.com/posts/parsing-csv-with-ruby/
+        skus[row[:sku]] ||= Array.new #this line initialized the array as an empty array
+        skus[row[:sku]] << row[:amount] #so this pushes the amount into the array of amounts that cooresponds to the sku. Key is sku
+        #and it has an array of amounts
       end
+
+      skus
+         
   end
 
-  def item
-   arr = []
-    if @amount == /(\ AUD\z)/
-      # finds the amounts that have AUD 
-      ##use nokogiri to convert AUDs to USDs??
-        doc = Nokogiri::XML(open('/Users/allisonsheren/projects/coder_night/lib/SAMPLE_RATES.xml'))
-        doc.xpath("//AUD").each do |node|
-          puts node.text
-        end 
+    currencies
+  end
+
+  def rates
+    Hash.from_xml(open('/Users/allisonsheren/projects/coder_night/lib/SAMPLE_RATES.xml').read) ["rates"] ["rate"]
+  end #calling from_xml on the Hash class, which returns a hash. so then we open the xml doc and then it parses it based on rates rate notations on the xml doc
+  #dig down those two elements deep and return a hash with all that information
+
+  def convert_amount (from, to)  #get to the point where I can print out the specific rate 
+    #based on converting directly from one thing to another
+    rate.each  
+      break 
+    end
+
+    #look through the list of froms and find the from currency you're looking for. then look through the list of to's
+    #to see if the to exists. if it does, apply the rate. If not, look until you find the correct to and then determine
+    #the from-to path and apply whatever conversion rates are necessary
+  end
+
+  def item #this will be specifically converting everything into USDs
+  # USD_amount = []
+    re = /(\ USD\z)/
+    string != "USD"
+    re.match(string)
+      # finds the amounts that aren't USD
     else
        puts @amount 
     end
-    arr << @amount #puts all the amounts (which are in USD) into an array
+    USD_amount << @amount #puts all the amounts (which are in USD) into an array
   end
 
   def total_sales 
@@ -45,8 +61,9 @@ class Puzzle1 #class is a method that defines puzzle and actually creates a cons
   end
 end
 
-p = Puzzle1.new
-p.find_item #calls the method. You need to set p (a variable that refers to an object Puzzle1) to Puzzle1.new. We created the class
-#puzzle1 which created a puzzle object but in order to call the method we have to creat an actual object to call it on 
-p.item 
-p.total_sales
+puzzle = Puzzle1.new
+puts puzzle.load_amounts["DM1182"] 
+puts puzzle.rates
+
+#puzzle.item 
+#puzzle.total_sales
